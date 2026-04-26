aa4ce3d3d63314bab98f73a1ac74bf1d49da9414
chsyam <syamkumar6845@gmail.com>
2026-04-26 00:36:25 -0600

    added web analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata = {
 	title: "Employee Onboarding Form | Lavance Group",
@@ -12,6 +13,7 @@ export default function RootLayout({ children }) {
 			className={`h-full antialiased`}
 		>
 			<body className="min-h-full flex flex-col">{children}</body>
+			<Analytics />
 		</html>
 	);
 }
